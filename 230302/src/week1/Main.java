a60af419680fd12b510aa676cccb3764768c7222
Chaerin Kim <decentralized0212@gmail.com>
2023-03-02 15:52:00 +0900

    Update Main.java

--- 230302/src/week1/Main.java
+++ 230302/src/week1/Main.java
@@ -3,7 +3,9 @@ package week1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        System.out.println("안녕하세요!");
+        System.out.println("객체지향프로그래밍을 시작합니다.");
 
 	}
 
